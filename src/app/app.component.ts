0334d4126448217496153a7ba05d7b4ae644ceef
1-ilnara <hajrulinailnara@gmail.comhajrulinailnara@gmail.com>
2026-05-25 21:41:05 +0000

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,25 +23,16 @@ export class AppComponent implements OnInit, OnDestroy {
 
 readonly companyName: string = 'РумТибет';
 
-  get locations(): string[] {
-    return ['Алтай', 'Кавказ', 'Памир', 'Урал'];
-  }
+  readonly availableDates: string[] = ['2026-05-10', '2026-06-11', '2026-07-12', '2026-08-13', '2026-09-14'];
 
-  get availableDates(): string[] {
-    return ['2026-05-10', '2026-06-11', '2026-07-12', '2026-08-13', '2026-09-14'];
-  }
-    
-  get participants(): number[] {
-    return [1, 2, 3, 4, 5];
-  }
+  readonly locations: string[] = ['Алтай', 'Кавказ', 'Памир', 'Урал'];
 
-  get collageImages(): string[] {
-  return ['tea_canyon', 'snake_canyon', 'snow_mobile', 'gree_hills'];
-  }
+  readonly participants: number[] = [1, 2, 3, 4, 5];
 
-  get galleryImages(): string[] {
-    return ['balloons_cappadocia', 'map_travel', 'dubai_hotel', 'tropical_coast', 'canyon_gallery', 'travel_items'];
-  }
+  readonly collageImages: string[] = ['tea_canyon', 'snake_canyon', 'snow_mobile', 'gree_hills'];
+ 
+  readonly galleryImages: string[] = ['balloons_cappadocia', 'map_travel', 'dubai_hotel', 'tropical_coast', 'canyon_gallery', 'travel_items'];
+  
   currentDate: string = '';  
   
   clickCount: number = 0;         
